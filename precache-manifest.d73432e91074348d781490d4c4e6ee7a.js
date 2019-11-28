5c318ca12ea34a237e8d55f3c37e5087bd6b5e87
Lou <lou.d.chaney@gmail.com>
2019-11-28 10:51:28 -0500

    Updates

---
+++ precache-manifest.d73432e91074348d781490d4c4e6ee7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98f4906b8b4a979ac4a555914422ac5",
+    "url": "/gh-pages-react-app/index.html"
+  },
+  {
+    "revision": "d576c049fb7929e7f082",
+    "url": "/gh-pages-react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6fb0c9ca5cbd80fe2a91",
+    "url": "/gh-pages-react-app/static/js/2.e1806175.chunk.js"
+  },
+  {
+    "revision": "d576c049fb7929e7f082",
+    "url": "/gh-pages-react-app/static/js/main.1c37a3dc.chunk.js"
+  },
+  {
+    "revision": "d982dc74f017aab07f40",
+    "url": "/gh-pages-react-app/static/js/runtime-main.11ae214f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/gh-pages-react-app/static/media/logo.25bf045c.svg"
+  }
+]);
